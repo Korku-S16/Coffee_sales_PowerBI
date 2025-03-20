8723611d855d9837d483687c414e0b2d95b8bf83
Korku-S16 <154559759+Korku-S16@users.noreply.github.com>
2025-03-21 01:02:13 +0530

    Create sales_comparisons.sql

---
+++ SQL_Queries/sales_comparisons.sql
@@ -0,0 +1,19 @@
+-- Comparing Daily Sales with Average Sales
+SELECT 
+    day_of_month,
+    CASE 
+        WHEN total_sales > avg_sales THEN 'Above Average'
+        WHEN total_sales < avg_sales THEN 'Below Average'
+        ELSE 'Average'
+    END AS sales_status,
+    total_sales
+FROM (
+    SELECT 
+        DAY(transaction_date) AS day_of_month,
+        SUM(unit_price * transaction_qty) AS total_sales,
+        AVG(SUM(unit_price * transaction_qty)) OVER () AS avg_sales
+    FROM coffee_shop_sales
+    WHERE MONTH(transaction_date) = 5
+    GROUP BY DAY(transaction_date)
+) AS sales_data
+ORDER BY day_of_month;
