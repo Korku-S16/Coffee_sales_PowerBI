dbe6d67d59a7ecb47be788db00b55e31951d6730
Korku-S16 <154559759+Korku-S16@users.noreply.github.com>
2025-03-21 00:53:20 +0530

    Create Sales Calculation.sql

---
+++ Scripts/Sales Calculation.sql
@@ -0,0 +1,18 @@
+--Total Sales Calculation:
+  
+SELECT ROUND(SUM(unit_price * transaction_qty)) AS Total_Sales
+FROM coffee_shop_sales
+WHERE MONTH(transaction_date) = 5;
+
+
+--Total Orders Calculation:
+
+SELECT COUNT(transaction_id) AS Total_Orders
+FROM coffee_shop_sales
+WHERE MONTH(transaction_date) = 5;
+
+--Total Quantity Sold:
+
+SELECT SUM(transaction_qty) AS Total_Quantity_Sold
+FROM coffee_shop_sales
+WHERE MONTH(transaction_date) = 5;
